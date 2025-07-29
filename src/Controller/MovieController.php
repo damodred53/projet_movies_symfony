71ebfcfa956c3297e5fa1dff0d82e6da86b46817
Florent <florent.guyard@live.fr>
2025-07-29 20:47:22 +0200

    suite configuration

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MovieController extends AbstractController
+{
+    #[Route('/', name: 'movie.index')]
+    public function index(): Response
+    {
+        return $this->render('movie/index.html.twig', [
+            'controller_name' => 'MovieController',
+
+        ]);
+    }
+}
