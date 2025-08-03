a47ebdad4e309d39ed320ece0c16248c0c04d2c2
Florent <florent.guyard@live.fr>
2025-08-03 13:51:59 +0200

    Fin de la partie intégration

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+use App\Service\Tmdb\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MovieController extends AbstractController
 {
+
+    public function __construct(
+        private readonly Client $client
+    ){
+
+    }
     #[Route('/', name: 'movie.index')]
     public function index(): Response
     {
         return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $this->client->popular(),
+        ]);
+    }
+    #[Route('/upcoming', name: 'movie.upcoming')]
+    public function upcoming(): Response
+    {
+        return $this->render('movie/upcoming.html.twig', [
+            'movies' => $this->client->upcoming(),
+        ]);
+    }
 
+    #[Route('/search', name: 'movie.search')]
+    public function search(#[MapQueryParameter] string $query): Response
+    {
+        return $this->render('movie/search.html.twig', [
+            'movies' => $this->client->query($query),
         ]);
     }
 }
